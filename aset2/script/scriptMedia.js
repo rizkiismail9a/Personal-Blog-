fdea8ce5f3304ce3a33d86ae782e1256c7d83447
rizkiismail <laksmimentari@gmail.com>
2023-02-28 21:13:44 +0700

    Responsipitas Day6

---
+++ aset2/script/scriptMedia.js
@@ -0,0 +1,15 @@
+let isHamburgerOpen = false;
+
+function openHamburger() {
+  let menuHamburger = document.getElementById("hamburger-navbar");
+  if (isHamburgerOpen) {
+    menuHamburger.style.display = "none";
+    isHamburgerOpen = false;
+  } else {
+    menuHamburger.style.display = "flex";
+    menuHamburger.style.flexDirection = "column";
+    menuHamburger.style.paddingTop = "15px";
+    menuHamburger.style.justifyContent = "space around";
+    isHamburgerOpen = true;
+  }
+}
