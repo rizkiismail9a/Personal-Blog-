7ab907a2abb7a7b02382744c194aa8a76288b13e
rizkiismail <laksmimentari@gmail.com>
2023-03-02 17:13:28 +0700

    Day-8 HOF

--- aset2/script/testimonial.js
+++ aset2/script/testimonial.js
@@ -63,9 +63,14 @@
 
 let testimonialData = [
   { nama: "Asep", quote: "Gokil!", rating: 4, image: `https://images.unsplash.com/photo-1600891406749-c179609d8475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJpYmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60` },
-  { nama: "Endang", quote: "Penipuan!", rating: 1, image: `https://images.unsplash.com/photo-1600891406749-c179609d8475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJpYmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60` },
-  { nama: "Cucu", quote: "GG Geming! Tapi Boong", rating: 3, image: `https://images.unsplash.com/photo-1600891406749-c179609d8475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJpYmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60` },
-  { nama: "Aang", quote: "Mantap abiez", rating: 5, image: `https://images.unsplash.com/photo-1600891406749-c179609d8475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJpYmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60` },
+  { nama: "Endang", quote: "Penipuan!", rating: 1, image: `https://images.unsplash.com/photo-1533645782036-997947a9d529?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80` },
+  {
+    nama: "Cucu",
+    quote: "GG Geming! Tapi Boong",
+    rating: 3,
+    image: `https://images.unsplash.com/photo-1627394679983-b6cff2bd5c25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHRyaWJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60`,
+  },
+  { nama: "Aang", quote: "Mantap abiez", rating: 5, image: `https://images.unsplash.com/photo-1537706388178-55c10865b82e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8dHJpYmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60` },
   { nama: "Usep Surasep", quote: "Keren, Bang!", rating: 4, image: `https://images.unsplash.com/photo-1600891406749-c179609d8475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJpYmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60` },
 ];
 
